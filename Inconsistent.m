4eec364bf03554eec408f39f738253e5ad365bbf
Matt <msklivas@live.com>
2020-10-30 20:11:41 -0400

    Add files via upload
    Updated approach for inconsistent equations

--- Inconsistent.m
+++ Inconsistent.m
@@ -1,10 +1,15 @@
 % Inconsistent equations
-clc;
-clear;
 
 x=[-2 -1 0 1];
 y=[3 -2 2 4];
-A=[1 0.01832 -0.90930;1 0.36788 -0.84147;1 1 0;1 0.36788 0.84147];
+n = length(x);
+m = length(x)-1;
+A = zeros(n,m)
+for i = 1:n
+  A(i,1) = 1;
+  A(i,2) = exp(-(x(i)^2));
+  A(i,3) = sin(x(i));
+end
 A_t=A'*A;
 A_tb = A'*y';
 a=A_t\A_tb
